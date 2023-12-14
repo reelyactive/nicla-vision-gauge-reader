8225137d980e2a01ced4b62152a682055308b134
jeffyactive <jeff@reelyactive.com>
2023-12-13 21:51:16 -0500

    Updated packet format comments

--- nicla/ra_ble.py
+++ nicla/ra_ble.py
@@ -27,7 +27,7 @@ ble.active(True)
 def send_value(value):
     # value is a percentage
     # we want a hex value btw 0 and 65535,
-    # then split into TWO 16 byte values (higher and lower)
+    # then split into TWO 8-bit values (higher and lower)
     bitvalue = int(value * .01 * 65535)
     high = (bitvalue >> 8)
     low = (bitvalue & 0x00FF)
@@ -36,7 +36,7 @@ def send_value(value):
     # 0:   0x05 = Length (not including the length byte itself)
     # 1:   0x16 = 16-bit service data (from GAP)
     # 2-3: 0x2af9 = 16-bit UUID of generic level characteristic (little endian)
-    # 4-5: 0x1234 = 16-bit generic level value (little endian) 4660 / 65535 = 7.11%
+    # 4-5: 0xLLHH = 16-bit generic level value (little endian)
     #_ADV_PAYLOAD = [ 0x05, 0x16, 0xf9, 0x2a, 0x34, 0x12 ]
     _ADV_PAYLOAD = [ 0x05, 0x16, 0xf9, 0x2a, low, high ]
     _ADV_INTERVAL_US = 500000 # 500000 microseconds = .5 seconds
